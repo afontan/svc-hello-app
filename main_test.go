56e069aef5327a70604f6ee661200c334748a9ae
Alejandro Fontán <alejandro.fontan@datadoghq.com>
2026-05-31 00:51:20 +0200

    test: add flaky external service call test

--- main_test.go
+++ main_test.go
@@ -5,7 +5,14 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
+
+func TestExternalServiceCall(t *testing.T) {
+	if time.Now().UnixNano()%2 == 0 {
+		t.Fatal("context deadline exceeded: upstream service did not respond")
+	}
+}
 
 func TestHealthz(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/healthz", nil)
